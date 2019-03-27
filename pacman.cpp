1821732eff5c2e131e7792cf1a822f1d0b468b4c
ntrnghia <ntnghia@apcs.vn>
2019-03-27 23:04:09 +0700

    Finish level 1 and 2

--- pacman.cpp
+++ pacman.cpp
@@ -1,27 +1,38 @@
-#include <algorithm>
+#include <unistd.h>     //use sleep()
 #include <fstream>
-#include <list>
+#include <iostream>
 #include <queue>
 #include <vector>
 using namespace std;
-void pacman(int **a, int n, int m, int sn, int sm) {
-    int dn, dm;
-    for (int r = 0; r < n; ++r) {
-        for (int c = 0; c < m; ++c) {
-            if (a[r][c] == 2) dn = r, dm = c;
+void print(vector<vector<int>> a, int m, int n, int cr, int cc) {
+    for (int r = 0; r < m; ++r) {
+        for (int c = 0; c < n; ++c) {
+            if (r == cr && c == cc)
+                cout << "* ";
+            else
+                cout << a[r][c] << ' ';
+        }
+        cout << endl;
+    }
+}
+void level_1_2(vector<vector<int>> a, int m, int n, int sm, int sn) {
+    int dm, dn;
+    for (int r = 0; r < m; ++r) {
+        for (int c = 0; c < n; ++c) {
+            if (a[r][c] == 2) dm = r, dn = c;
         }
     }
-    vector<vector<int>> b(n, vector<int>(m, INT_MAX));
-    vector<vector<pair<int, int>>> pre(n, vector<pair<int, int>>(m, {-1, -1}));
+    vector<vector<int>> b(m, vector<int>(n, INT_MAX));
+    vector<vector<pair<int, int>>> pre(m, vector<pair<int, int>>(n));
     queue<vector<int>> loc;
-    loc.push({dn, dm});
-    b[dn][dm] = 0;
+    loc.push({dm, dn});
+    b[dm][dn] = 0;
     int dr[]{1, 0, -1, 0}, dc[]{0, 1, 0, -1};
     while (!loc.empty()) {
         int cr = loc.front()[0], cc = loc.front()[1];
         for (int i = 0; i < 4; ++i) {
             int nr = cr + dr[i], nc = cc + dc[i];
-            if (nr >= 0 && nr < n && nc >= 0 && nc < m && a[nr][nc] == 0 &&
+            if (nr >= 0 && nr < m && nc >= 0 && nc < n && a[nr][nc] == 0 &&
                 b[nr][nc] > b[cr][cc] + 1) {
                 loc.push({nr, nc});
                 b[nr][nc] = b[cr][cc] + 1;
@@ -30,34 +41,36 @@ void pacman(int **a, int n, int m, int sn, int sm) {
         }
         loc.pop();
     }
-    for (int cr = sn, cc = sm; pre[cr][cc].first != -1;) {
+    int path_length = 0;
+    pre[dm][dn] = {-1, -1};
+    for (int cr = sm, cc = sn; cr != -1 || cc != -1; ++path_length) {
+        system("cls");
+        print(a, m, n, cr, cc);
         int tmpr = pre[cr][cc].first;
         int tmpc = pre[cr][cc].second;
-        a[tmpr][tmpc] = 5;
         cr = tmpr;
         cc = tmpc;
+        cout << "Path length: " << path_length << endl;
+        cout << "Point: ";
+        if (cr == -1 && cc == -1)
+            cout << 11 - path_length;
+        else
+            cout << -path_length;
+        sleep(1);
     }
-    ofstream fout("output.txt");
-    for (int r = 0; r < n; ++r) {
-        for (int c = 0; c < m; ++c) {
-            fout << a[r][c] << ' ';
-        }
-        fout << endl;
-    }
+    cin.get();
 }
+void level_3(int **a, int m, int n, int sm, int sn) {}
 int main() {
     ifstream fin("input.txt");
-    int n, m, sn, sm;
-    fin >> n >> m;
-    int **a = new int *[n];
-    for (int i = 0; i < n; ++i) a[i] = new int[m];
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
+    int m, n, sm, sn;
+    fin >> m >> n;
+    vector<vector<int>> a(m, vector<int>(n));
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
             fin >> a[i][j];
         }
     }
-    fin >> sn >> sm;
-    pacman(a, n, m, sn, sm);
-    for (int i = 0; i < n; ++i) delete[] a[i];
-    delete[] a;
+    fin >> sm >> sn;
+    level_1_2(a, m, n, sm, sn);
 }
